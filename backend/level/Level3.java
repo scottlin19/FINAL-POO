c174b205d27a7616142086c561a3e9db2c947894
scottlin19 <scott.lin099@gmail.com>
2019-06-25 19:01:34 -0300

    level3

--- backend/level/Level3.java
+++ backend/level/Level3.java
@@ -1,6 +1,11 @@
 package game.backend.level;
 
+import game.backend.GameState;
 
 public class Level3 extends Level {
+
+    private class Level3State extends GameState{
+        private int maxMoves;
+    }
 
 }
