0a875974ccea75a8ebc0715ba1a1e16bee935431
scottlin19 <scott.lin099@gmail.com>
2019-06-25 17:31:42 -0300

    level selector

--- frontend/GameApp.java
+++ frontend/GameApp.java
@@ -6,10 +6,12 @@ import game.backend.level.Level2;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GameApp extends Application {
@@ -21,23 +23,25 @@ public class GameApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
-		Label label = new Label("select level: ");
+		Label label = new Label("Select level:");
 		Button button = new Button("Level 1");
 		Button button1 = new Button(("Level 2"));
 		Button button2 = new Button("Level 3");
-		HBox hBox1 = new HBox(5);  // layout
-		Scene scene1 = new Scene(hBox1,800,600);
-		hBox1.getChildren().addAll(label,button,button1,button2);
+		VBox vBox = new VBox(5);  // layout
+		HBox hBox = new HBox(5);
+		Scene scene1 = new Scene(vBox,800,600);
+		hBox.getChildren().addAll(button,button1,button2);
+		hBox.setAlignment(Pos.CENTER);
+		vBox.getChildren().addAll(label,hBox);
 		primaryStage.setScene(scene1);
-
-		CandyGame game = new CandyGame(Level2.class);
-		CandyFrame frame = new CandyFrame(game);
-		Scene scene = new Scene(frame);
-		primaryStage.setResizable(false);
+		vBox.setAlignment(Pos.CENTER);
 
 		button.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
+				CandyGame game = new CandyGame(Level1.class);
+				CandyFrame frame = new CandyFrame(game);
+				Scene scene = new Scene(frame);
 				primaryStage.setScene(scene);
 			}
 		});
@@ -45,6 +49,10 @@ public class GameApp extends Application {
 		button1.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
+
+				CandyGame game = new CandyGame(Level2.class);
+				CandyFrame frame = new CandyFrame(game);
+				Scene scene = new Scene(frame);
 				primaryStage.setScene(scene);
 			}
 		});
@@ -52,9 +60,10 @@ public class GameApp extends Application {
 		button2.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				primaryStage.setScene(scene);
 			}
 		});
+
+		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
 
