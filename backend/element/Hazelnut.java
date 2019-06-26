f44880c15df5470521ac9cb8a21badef8209f3b6
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2019-06-26 20:42:52 -0300

    Update Hazelnut.java

--- backend/element/Hazelnut.java
+++ backend/element/Hazelnut.java
@@ -1,14 +1,14 @@
 package game.backend.element;
 
-public class Hazelnut extends Element {
-    @Override
-    public boolean isMovable() {
-        return true;
+public class Hazelnut extends Fruit {
+  public Hazelnut(FruitType type) {
+        super(type);
     }
 
     @Override
     public String getKey() {
         return "HAZELNUT";
     }
+
 
 }
