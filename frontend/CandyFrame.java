a68dc25beeac147600756db25ad9aee20b48dbbf
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2019-06-26 00:06:31 -0300

    Update CandyFrame.java

--- frontend/CandyFrame.java
+++ frontend/CandyFrame.java
@@ -47,9 +47,8 @@ public class CandyFrame extends VBox {
 						Cell cell = CandyFrame.this.game.get(i, j);
 						Element element = cell.getContent();
 						Image image = images.getImage(element);
-
-						timeLine.getKeyFrames().add(new KeyFrame(frameTime, e -> boardPanel.setImage(finalI, finalJ, null,false)));
-						timeLine.getKeyFrames().add(new KeyFrame(frameTime, e -> boardPanel.setImage(finalI, finalJ, image,cell.isGolden())));
+timeLine.getKeyFrames().add(new KeyFrame(frameTime, e -> boardPanel.setImage(finalI, finalJ, null,cell)));
+						timeLine.getKeyFrames().add(new KeyFrame(frameTime, e -> boardPanel.setImage(finalI, finalJ, image,cell)));
 					}
 					frameTime = frameTime.add(frameGap);
 				}
