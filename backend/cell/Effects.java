055a1956a076e9eac81b52ebe645a402bf5e4191
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2019-06-26 17:02:04 -0300

    Create Effects.java

---
+++ backend/cell/Effects.java
@@ -0,0 +1,23 @@
+package game.backend.cell;
+
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
+import javafx.scene.paint.Color;
+
+public enum Effects {
+
+    GOLDEN{
+        @Override
+        public Effect getEffect() {
+            Light.Distant spotLight = new Light.Distant();
+            spotLight.setColor(Color.YELLOW);
+            spotLight.setElevation(100);
+            return new Lighting(spotLight);
+        }
+    };
+
+    public abstract Effect getEffect();
+
+
+}
