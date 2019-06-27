98bd32f8041e187d2f6e1a92124613c25fee9003
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2019-06-27 15:20:24 -0300

    Update Level4.java

--- backend/level/Level4.java
+++ backend/level/Level4.java
@@ -29,12 +29,19 @@ public class Level4 extends Grid {
         wallCell = new Cell(this);
         wallCell.setContent(new Wall());
         candyAndFruitGenCell = new CandyAndFruitGeneratorCell(this);
+        CandyGeneratorCell candyGenCell = new CandyGeneratorCell(this);
 
         //corners
         g()[0][0].setAround(candyAndFruitGenCell, g()[1][0], wallCell, g()[0][1]);
         g()[0][SIZE-1].setAround(candyAndFruitGenCell, g()[1][SIZE-1], g()[0][SIZE-2], wallCell);
         g()[SIZE-1][0].setAround(g()[SIZE-2][0], wallCell, wallCell, g()[SIZE-1][1]);
         g()[SIZE-1][SIZE-1].setAround(g()[SIZE-2][SIZE-1], wallCell, g()[SIZE-1][SIZE-2], wallCell);
+
+
+        //upper line cells
+        for (int j = 1; j < SIZE-1; j++) {
+            g()[0][j].setAround(candyAndFruitGenCell,g()[1][j],g()[0][j-1],g()[0][j+1]);
+        }
 
         //upper line cells
         for (int j = 1; j < SIZE-1; j++) {
@@ -57,6 +64,9 @@ public class Level4 extends Grid {
             for (int j = 1; j < SIZE-1; j++) {
                 g()[i][j].setAround(g()[i-1][j],g()[i+1][j],g()[i][j-1],g()[i][j+1]);
             }
+        }
+        for(int i = 0; i < SIZE-1;i++){
+            getCell(SIZE-1,i).setContent(candyGenCell.getContent());
         }
     }
 
