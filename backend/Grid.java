c0ea52ecf44427c9c9cd2907a990724cbefb179a
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2019-06-27 15:21:07 -0300

    Update Grid.java

--- backend/Grid.java
+++ backend/Grid.java
@@ -79,9 +79,9 @@ public abstract class Grid {
 	}
 	
 	public void clearContent(int i, int j) {
-		if(!g[i][j].getContent().isFruit()){
+	
 		g[i][j].clearContent();
-		}
+	
 	}
 	
 	public void setContent(int i, int j, Element e) {
