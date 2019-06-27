0367c3e7cf5ab5add643268a71bc328087cf7cc0
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2019-06-26 21:49:17 -0300

    Update MoveMaker.java

--- backend/move/MoveMaker.java
+++ backend/move/MoveMaker.java
@@ -22,13 +22,14 @@ public class MoveMaker {
 
 	private void initMap(){
 		map = new HashMap<>();
+		
 		map.put(new Candy().getKey() + new Candy().getKey(), new CandyMove(grid));
 		map.put(new Candy().getKey() + new HorizontalStripedCandy().getKey(), new CandyMove(grid));
 		map.put(new Candy().getKey() + new VerticalStripedCandy().getKey(), new CandyMove(grid));
 		map.put(new Candy().getKey() + new WrappedCandy().getKey(), new CandyMove(grid));
 		map.put(new Candy().getKey() + new Bomb().getKey(), new BombMove(grid));
 
-		map.put(new Candy().getKey() + new Fruit().getKey(), new CandyMove(grid));
+		map.put(new Candy().getKey() + new Fruit().getKey(), new FruitMove(grid));
 	
 		map.put(new HorizontalStripedCandy().getKey() + new Candy().getKey(), new CandyMove(grid));
 		map.put(new HorizontalStripedCandy().getKey() + new HorizontalStripedCandy().getKey(), new TwoStripedMove(grid));
@@ -36,7 +37,7 @@ public class MoveMaker {
 		map.put(new HorizontalStripedCandy().getKey() + new WrappedCandy().getKey(), new WrappedStripedMove(grid));
 		map.put(new HorizontalStripedCandy().getKey() + new Bomb().getKey(), new BombStrippedMove(grid));
 
-		map.put(new HorizontalStripedCandy().getKey() + new Bomb().getKey(), new CandyMove(grid));
+		map.put(new HorizontalStripedCandy().getKey() + new Bomb().getKey(), new FruitMove(grid));
 
 
 		map.put(new VerticalStripedCandy().getKey() + new Candy().getKey(), new CandyMove(grid));
@@ -45,7 +46,7 @@ public class MoveMaker {
 		map.put(new VerticalStripedCandy().getKey() + new WrappedCandy().getKey(), new WrappedStripedMove(grid));
 		map.put(new VerticalStripedCandy().getKey() + new Bomb().getKey(), new BombStrippedMove(grid));
 
-		map.put(new VerticalStripedCandy().getKey() + new Fruit().getKey(), new CandyMove(grid));
+		map.put(new VerticalStripedCandy().getKey() + new Fruit().getKey(), new FruitMove(grid));
 
 		map.put(new WrappedCandy().getKey() + new Candy().getKey(), new CandyMove(grid));
 		map.put(new WrappedCandy().getKey() + new HorizontalStripedCandy().getKey(), new WrappedStripedMove(grid));
@@ -53,7 +54,7 @@ public class MoveMaker {
 		map.put(new WrappedCandy().getKey() + new WrappedCandy().getKey(), new TwoWrappedMove(grid));
 		map.put(new WrappedCandy().getKey() + new Bomb().getKey(), new BombWrappedMove(grid));
 
-		map.put(new WrappedCandy().getKey() + new Fruit().getKey(), new CandyMove(grid));
+		map.put(new WrappedCandy().getKey() + new Fruit().getKey(), new FruitMove(grid));
 
 		map.put(new Bomb().getKey() + new Candy().getKey(), new BombMove(grid));
 		map.put(new Bomb().getKey() + new HorizontalStripedCandy().getKey(), new BombStrippedMove(grid));
@@ -61,13 +62,13 @@ public class MoveMaker {
 		map.put(new Bomb().getKey() + new WrappedCandy().getKey(), new BombWrappedMove(grid));
 		map.put(new Bomb().getKey() + new Bomb().getKey(), new TwoBombMove(grid));
 
-		map.put(new Bomb().getKey() + new Fruit().getKey(), new CandyMove(grid));
+		map.put(new Bomb().getKey() + new Fruit().getKey(), new FruitMove(grid));
 
-		map.put(new Fruit().getKey() + new Candy().getKey(), new CandyMove(grid));
-		map.put(new Fruit().getKey() + new HorizontalStripedCandy().getKey(), new CandyMove(grid));
-		map.put(new Fruit().getKey() + new VerticalStripedCandy().getKey(), new CandyMove(grid));
-		map.put(new Fruit().getKey() + new WrappedCandy().getKey(), new CandyMove(grid));
-		map.put(new Fruit().getKey() + new Bomb().getKey(), new CandyMove(grid));
+		map.put(new Fruit().getKey() + new Candy().getKey(), new FruitMove(grid));
+		map.put(new Fruit().getKey() + new HorizontalStripedCandy().getKey(), new FruitMove(grid));
+		map.put(new Fruit().getKey() + new VerticalStripedCandy().getKey(), new FruitMove(grid));
+		map.put(new Fruit().getKey() + new WrappedCandy().getKey(), new FruitMove(grid));
+		map.put(new Fruit().getKey() + new Bomb().getKey(), new FruitMove(grid));
 		map.put(new Fruit().getKey() + new Fruit().getKey(), new FruitMove(grid));
 	}
 	
