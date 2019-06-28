815b939ffd9abc82d38d36ba2d0820a479fe5c56
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2019-06-27 23:33:18 -0300

    Update BoardPanel.java

--- frontend/BoardPanel.java
+++ frontend/BoardPanel.java
@@ -2,30 +2,31 @@ package game.frontend;
 
 import game.backend.cell.Cell;
 import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 
 
 public class BoardPanel extends TilePane {
 
-	private StackPane[][] cells;
+	private ImageView[][] cells;
 
 	public BoardPanel(final int rows, final int columns, final int cellSize) {
 		setPrefRows(rows);
 		setPrefColumns(columns);
 		setPrefTileHeight(cellSize);
 		setPrefTileWidth(cellSize);
-		this.cells = new StackPane[rows][columns];
+		this.cells = new ImageView[rows][columns];
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
-				cells[i][j] = new StackPane();
+				cells[i][j] = new ImageView();
 				getChildren().add(cells[i][j]);
 			}
 		}
 	}
 	
 	public void setImage(int row, int column, Image image, Cell cell) {
-		cells[row][column].getChildren().add(cell.getImage(image));
+		cells[row][column].setEffect(cell.getEffect());
+		cells[row][column].setImage(image);
 
 	}
 
