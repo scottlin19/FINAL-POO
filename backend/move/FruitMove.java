ec3369ffff57a727787e5a920987210f96afea0d
MatiMonaco <45739403+MatiMonaco@users.noreply.github.com>
2019-07-05 19:52:49 -0300

    Update FruitMove.java

--- backend/move/FruitMove.java
+++ backend/move/FruitMove.java
@@ -1,9 +1,43 @@
 package game.backend.move;
 
+import game.backend.Figure;
+import game.backend.FigureDetector;
 import game.backend.Grid;
 
-public class FruitMove extends CandyMove {
+public class FruitMove extends Move {
+
+
+    private Figure f1;
+    private Figure f2;
+
+    private FigureDetector detector;
+    private Grid grid;
+
+
     public FruitMove(Grid grid) {
         super(grid);
+        this.grid = grid;
+    }
+
+    @Override
+    public boolean internalValidation() {
+        this.detector = new FigureDetector(grid);
+        if(grid.get(i1,j1).canFormFigures()) {
+            f1 = detector.checkFigure(i1, j1);
+        }
+        if(grid.get(i2,j2).canFormFigures()) {
+            f2 = detector.checkFigure(i2, j2);
+        }
+        return f1 != null || f2 != null;
+    }
+
+    @Override
+    public void removeElements() {
+        if (f1 != null) {
+            detector.removeFigure(i1, j1, f1);
+        }
+        if (f2 != null) {
+            detector.removeFigure(i2, j2, f2);
+        }
     }
 }
